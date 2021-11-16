f011ff10539231066f7b224d65153ffd19b4a290
Ciara Martin <rileymartin52894@gmail.com>
2021-11-15 19:36:44 -0800

    Added euler problem 5

---
+++ src/Euler5.hs
@@ -0,0 +1,8 @@
+import           Lib (lcmSequence)
+
+{-
+>>> euler5
+232792560
+-}
+euler5 :: Integer
+euler5 = lcmSequence !! 20
--- src/Lib.hs
+++ src/Lib.hs
@@ -1,7 +1,55 @@
-module Lib
-    ( someFunc
-    ) where
+module Lib where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+{-
+>>> take 10 primes
+[2,3,5,7,11,13,17,19,23,29]
+-}
+primes :: [Integer]
+primes = sieve [2..]
+        where
+            sieve (p:xs) = p : sieve [x | x <- xs, rem x p > 0]
+            sieve []     = []
 
+{-
+>>> primeFactors 60
+[2,3,5]
+
+>>> primeFactors 600851475143
+[71,839,1471,6857]
+-}
+primeFactors :: Integer -> [Integer]
+primeFactors n = factor n primes
+                where
+                    factor _ [] = []
+                    factor m (p:xs)
+                        | p > m = []
+                        | mod m p == 0 = p:factor (div m p) xs
+                        | otherwise = factor m xs
+
+{-
+>>> multiplePrimeFactors 60
+[2,2,3,5]
+
+>>> multiplePrimeFactors 232792560
+[2,2,2,2,3,3,5,7,11,13,17,19]
+-}
+multiplePrimeFactors :: Integer -> [Integer]
+multiplePrimeFactors n = factor n primes
+                where
+                    factor _ [] = []
+                    factor m (p:xs)
+                        | p > m = []
+                        | mod m p == 0 = p:factor (div m p) (p:xs)
+                        | otherwise = factor m xs
+
+merge :: [a] -> [a] -> [a]
+merge xs     []     = xs
+merge []     ys     = ys
+merge (x:xs) (y:ys) = x : y : merge xs ys
+
+{-
+>>> lcmSequence !! 20
+232792560
+-}
+lcmSequence :: [Integer]
+lcmSequence = scanl1 (\l n -> l * div n (gcd l n)) [1..]
